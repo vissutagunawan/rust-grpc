0acaedca34d0b55cbf8b0d4b8fcdee7b580d04c0
vissutagunawan <vglim3653@gmail.com>
2025-04-22 15:48:15 +0700

    Create base skeleton of Rust gRPC.

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(true)
+        .compile(&["proto/services.proto"], &["proto"])
+        .unwrap();
+
+    Ok(())
+}
