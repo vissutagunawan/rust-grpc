11fdef1086a6a7574462e0009a5b0e69f1eb213f
vissutagunawan <vglim3653@gmail.com>
2025-04-22 16:24:07 +0700

    Chat Service Implementation

--- src/grpc_client.rs
+++ src/grpc_client.rs
@@ -7,6 +7,11 @@ use services::{
     transaction_service_client::TransactionServiceClient, ChatMessage, PaymentRequest,
     TransactionRequest,
 };
+use tonic::transport::Channel;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio::sync::mpsc::{Sender, Receiver};
+use tokio::io::{self, AsyncBufReadExt};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -27,6 +32,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = transaction_client.get_transaction_history(request).await?.into_inner();
     while let Some(transaction) = stream.message().await? {
         println!("Transaction: {:?}", transaction);
+    }
+
+    let channel = Channel::from_static("http://[::1]:50051").connect().await?;
+    let mut client = ChatServiceClient::new(channel);
+    let (tx, rx): (Sender<ChatMessage>, Receiver<ChatMessage>) = mpsc::channel(32);
+
+    tokio::spawn(async move {
+        let stdin = io::stdin();
+        let mut reader = io::BufReader::new(stdin).lines();
+
+        while let Ok(Some(line)) = reader.next_line().await {
+            if line.trim().is_empty() {
+                continue;
+            }
+            let message = ChatMessage {
+                user_id: "user_123".to_string(),
+                message: line,
+            };
+
+            if tx.send(message).await.is_err(){
+                eprintln!("Failed to send message to server.");
+                break;
+            }
+        }
+    });
+
+    let request = tonic::Request::new(ReceiverStream::new(rx));
+    let mut response_stream = client.chat(request).await?.into_inner();
+
+    while let Some(response) = response_stream.message().await? {
+        println!("Server says: {:?}", response);
     }
 
     Ok(())
